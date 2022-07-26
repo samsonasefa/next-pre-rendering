4b0c0acd133ee509941827922270c2fb5536c315
Samson <samson.asefa@tobania.be>
2022-07-26 06:36:15 +0300

    basic ssr

---
+++ pages/news/index.js
@@ -0,0 +1,25 @@
+export default function NewsList({ articles }) {
+  return (
+    <>
+      <h2>List of articles</h2>
+      {articles.map((article) => {
+        return (
+          <div key={article.id}>
+            <h4>{article.title}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch("http://localhost:4000/news");
+  const data = await res.json();
+
+  return {
+    props: {
+      articles: data,
+    },
+  };
+}
